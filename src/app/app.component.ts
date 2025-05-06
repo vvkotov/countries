7b401f8fdfdde242ee1cbfd8b1c284746e76489e
Vitaliy Kotov <vitaliy.kotov@fairmarkit.com>
2025-05-06 17:58:45 +0200

    Add variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'countries';
